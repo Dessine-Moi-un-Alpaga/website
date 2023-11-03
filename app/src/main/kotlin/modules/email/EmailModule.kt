15f50c5362494fce93611610dcbc319a2d0bdfa9
Gilles Gosuin <gilles.gosuin@g2consulting.be>
2023-11-03 21:17:11 +0100

    refactor: replace the fat sendgrid client with a simpler one

--- app/src/main/kotlin/modules/email/EmailModule.kt
+++ app/src/main/kotlin/modules/email/EmailModule.kt
@@ -1,7 +1,6 @@
 package be.alpago.website.modules.email
 
 import be.alpago.website.libs.environment.Environment
-import com.sendgrid.SendGrid
 import io.ktor.server.application.Application
 import org.koin.dsl.module
 import org.koin.ktor.plugin.koin
@@ -9,12 +8,7 @@ import org.koin.ktor.plugin.koin
 fun sendGridEmailService() = module {
     single<EmailService> {
         val environment by inject<Environment>()
-        val sendGridApiKey = environment.sendGridApiKey
-        val sendGrid = SendGrid(sendGridApiKey)
-        SendGridEmailService(
-            environment = environment,
-            sendGrid = sendGrid
-        )
+        SendGridEmailService(environment)
     }
 }
 
