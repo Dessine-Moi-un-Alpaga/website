8d8af6a469e66e517a60ba3acc9f34bbb59b8667
Gilles Gosuin <gilles.gosuin@g2consulting.be>
2024-03-18 08:59:00 +0100

    refactor: remove useless repository resolution when handling management route requests

--- app/src/main/kotlin/interfaces/ktor/ManagementRoutes.kt
+++ app/src/main/kotlin/interfaces/ktor/ManagementRoutes.kt
@@ -5,7 +5,6 @@ import be.alpago.website.libs.domain.ports.AggregateRootNotFound
 import be.alpago.website.libs.domain.ports.Repository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
-import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
 import io.ktor.server.request.receive
@@ -15,18 +14,14 @@ import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.put
 import io.ktor.server.routing.routing
-import io.ktor.util.pipeline.PipelineContext
-
-typealias RepositoryProvider<T> = (PipelineContext<Unit, ApplicationCall>) -> Repository<T>
 
 const val ID = "id"
 
 inline fun <reified T : AggregateRoot> Route.findAllRoute(
     path: String,
-    crossinline repositoryProviderFor: RepositoryProvider<T>
+    repository: Repository<T>,
 ) {
     get(path) {
-        val repository = repositoryProviderFor(this)
         call.respond(
             repository.findAll()
         )
@@ -35,7 +30,7 @@ inline fun <reified T : AggregateRoot> Route.findAllRoute(
 
 inline fun <reified T : AggregateRoot> Route.getRoute(
     path: String,
-    crossinline repositoryProviderFor: RepositoryProvider<T>
+    repository: Repository<T>,
 ) {
     get("${path}/{$ID}") {
         val id = call.parameters[ID]
@@ -43,8 +38,6 @@ inline fun <reified T : AggregateRoot> Route.getRoute(
         if (id == null) {
             call.response.status(HttpStatusCode.BadRequest)
         } else {
-            val repository = repositoryProviderFor(this)
-
             try {
                 val aggregateRoot = repository.get(id)
                 call.respond(aggregateRoot)
@@ -57,11 +50,10 @@ inline fun <reified T : AggregateRoot> Route.getRoute(
 
 inline fun <reified T : AggregateRoot> Route.putRoute(
     path: String,
-    crossinline repositoryProviderFor: RepositoryProvider<T>
+    repository: Repository<T>,
 ) {
     put(path) {
         val aggregateRoot = call.receive<T>()
-        val repository = repositoryProviderFor(this)
         repository.create(aggregateRoot)
         call.response.status(HttpStatusCode.OK)
     }
@@ -69,10 +61,9 @@ inline fun <reified T : AggregateRoot> Route.putRoute(
 
 inline fun <reified T : AggregateRoot> Route.deleteAllRoute(
     path: String,
-    crossinline repositoryProviderFor: RepositoryProvider<T>
+    repository: Repository<T>,
 ) {
     delete(path) {
-        val repository = repositoryProviderFor(this)
         repository.deleteAll()
         call.response.status(HttpStatusCode.OK)
     }
@@ -80,7 +71,7 @@ inline fun <reified T : AggregateRoot> Route.deleteAllRoute(
 
 inline fun <reified T : AggregateRoot> Route.deleteRoute(
     path: String,
-    crossinline repositoryProviderFor: RepositoryProvider<T>
+    repository: Repository<T>,
 ) {
     delete("${path}/{$ID}") {
         val id = call.parameters[ID]
@@ -88,8 +79,6 @@ inline fun <reified T : AggregateRoot> Route.deleteRoute(
         if (id == null) {
             call.response.status(HttpStatusCode.BadRequest)
         } else {
-            val repository = repositoryProviderFor(this)
-
             try {
                 repository.delete(id)
                 call.response.status(HttpStatusCode.OK)
@@ -102,22 +91,15 @@ inline fun <reified T : AggregateRoot> Route.deleteRoute(
 
 inline fun <reified T : AggregateRoot> Application.managementRoutes(
     path: String,
-    crossinline repositoryProvider: RepositoryProvider<T>
+    repository: Repository<T>
 ) {
     routing {
         authenticate {
-            findAllRoute(path, repositoryProvider)
-            getRoute(path, repositoryProvider)
-            putRoute(path, repositoryProvider)
-            deleteAllRoute(path, repositoryProvider)
-            deleteRoute(path, repositoryProvider)
+            findAllRoute(path, repository)
+            getRoute(path, repository)
+            putRoute(path, repository)
+            deleteAllRoute(path, repository)
+            deleteRoute(path, repository)
         }
     }
-}
-
-inline fun <reified T : AggregateRoot> Application.managementRoutes(
-    path: String,
-    repository: Repository<T>
-) {
-    managementRoutes(path) { repository }
 }
