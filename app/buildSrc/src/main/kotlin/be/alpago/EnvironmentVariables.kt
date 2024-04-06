934fa9acf70b42f48543293f061ca96aee666682
Gilles Gosuin <gilles.gosuin@g2consulting.be>
2024-04-06 11:01:07 +0200

    build: fix Gradle property retrieval

--- app/buildSrc/src/main/kotlin/be/alpago/EnvironmentVariables.kt
+++ app/buildSrc/src/main/kotlin/be/alpago/EnvironmentVariables.kt
@@ -4,28 +4,18 @@ import org.gradle.api.Project
 import org.gradle.process.ProcessForkOptions
 
 fun ProcessForkOptions.environmentVariables(project: Project) {
-    project.property("bucket")?.let {
-        environment("DMUA_BASE_ASSET_URL",  "https://storage.googleapis.com/${it}")
-    }
+    val credentials = project.property("credentials")
+    val devBucket = project.property("devBucket")
+    val firestorePort = project.property("firestorePort")
+    val googleProject = project.property("googleProject")
+    val sendGridApiKey = project.property("sendGridApiKey")
 
-    project.property("credentials")?.let {
-        environment("DMUA_CREDENTIALS", it)
-    }
-
+    environment("DMUA_BASE_ASSET_URL", "https://storage.googleapis.com/${devBucket}")
+    environment("DMUA_CREDENTIALS", credentials)
     environment("DMUA_EMAIL_ADDRESS", "contact@dessinemoiunalpaga.com")
     environment("DMUA_ENVIRONMENT", "local")
-
-    project.property("firestorePort")?.let {
-        environment("DMUA_FIRESTORE_URL","http://localhost:${it}")
-    }
-
-    project.property("googleProject")?.let {
-        environment("DMUA_PROJECT", it)
-    }
-
-    project.property("sendGridApiKey")?.let {
-        environment("DMUA_SEND_GRID_API_KEY", it)
-    }
-
+    environment("DMUA_FIRESTORE_URL", "http://localhost:${firestorePort}")
+    environment("DMUA_PROJECT", googleProject)
+    environment("DMUA_SEND_GRID_API_KEY", sendGridApiKey)
     environment("DMUA_TEST", true)
 }
