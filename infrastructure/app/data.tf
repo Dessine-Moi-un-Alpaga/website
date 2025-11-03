00d465b2b1a75190f570a8fff19d4aa0d9b7f527
Gilles Gosuin <gilles.gosuin@gmail.com>
2025-11-03 06:15:43 +0100

    refactor: remove runtime references to the sendgrid environment variable

--- infrastructure/app/data.tf
+++ infrastructure/app/data.tf
@@ -9,10 +9,6 @@ data "google_secret_manager_secret" "credentials_secret" {
   secret_id = "credentials"
 }
 
-data "google_secret_manager_secret" "send_grid_api_key_secret" {
-  secret_id = "send-grid-api-key"
-}
-
 data "google_secret_manager_secret" "smtp_server_password_secret" {
   secret_id = "smtp-server-password"
 }
