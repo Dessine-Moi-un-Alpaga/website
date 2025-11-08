d758447305c3a8c37dd7b1b5f79f89acbbaa44b7
Gilles Gosuin <gilles.gosuin@gmail.com>
2025-11-08 05:12:05 +0100

    ci: add missing required buildScan configuration

--- app/settings.gradle.kts
+++ app/settings.gradle.kts
@@ -1,1 +1,14 @@
+plugins {
+    id("com.gradle.develocity") version("4.2.2")
+}
+
 rootProject.name = "website"
+
+develocity {
+    buildScan {
+        publishing.onlyIf { false }
+
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
