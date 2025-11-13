ae76bdf5cb6a42edefb8e0c699c28fa4ef26644a
Gilles Gosuin <gilles.gosuin@gmail.com>
2025-11-13 09:27:22 +0100

    refactor: revert webjar changes as they are redundant with the native build

--- app/buildSrc/src/main/kotlin/webjars.gradle.kts
+++ app/buildSrc/src/main/kotlin/webjars.gradle.kts
@@ -1,38 +1,6 @@
 val taskName = "explodeWebjars"
 val webjars by configurations.creating
 
-val filters = mapOf(
-    "escape-velocity" to setOf(
-        "css/**",
-        "js/**",
-    ),
-    "font-awesome" to setOf(
-        "css/brands.min.css",
-        "css/fontawesome.min.css",
-        "css/solid.min.css",
-        "webfonts/*",
-    ),
-    "jquery.dropotron" to setOf(
-        "jquery.dropotron.min.js",
-    ),
-    "jquery" to setOf(
-        "jquery.min.js",
-    ),
-    "photoswipe" to setOf(
-        "dist/photoswipe.css",
-        "dist/photoswipe.esm.min.js",
-        "dist/photoswipe-lightbox.esm.min.js",
-    ),
-    "responsive-tools" to setOf(
-        "dist/breakpoints.min.js",
-        "dist/browser.min.js",
-    ),
-    "toastr" to setOf(
-        "build/toastr.min.css",
-        "build/toastr.min.js",
-    )
-)
-
 tasks.register<Copy>(taskName) {
     description = "Explodes WebJars to the build resources."
     group = "build"
@@ -43,15 +11,8 @@ tasks.register<Copy>(taskName) {
             val artifact = config.resolvedArtifacts.find {
                 "${it.file}" == jar.absolutePath
             }
-
-            val artifactName = artifact!!.name
-            val artifactVersion = artifact.moduleVersion.id.version
-            println("${artifactName}:${artifactVersion}")
-            val filtersForArtifact = filters[artifactName] ?: emptySet()
-
-            filtersForArtifact.forEach { filter ->
-                include("META-INF/resources/webjars/${artifactName}/${artifactVersion}/${filter}")
-            }
+            exclude("META-INF/maven/**")
+            exclude("META-INF/MANIFEST.MF")
             eachFile {
                 val segments = relativePath.segments.drop(5).toTypedArray()
                 relativePath = RelativePath(true, artifact!!.name, *segments)
