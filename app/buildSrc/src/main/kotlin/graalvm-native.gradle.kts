f1fc9178b0745d6635bea5f69956000df5ab41eb
Gilles Gosuin <gilles.gosuin@gmail.com>
2025-11-13 06:41:31 +0100

    fix: add missing build-time initializer for slf4j-simple

--- app/buildSrc/src/main/kotlin/graalvm-native.gradle.kts
+++ app/buildSrc/src/main/kotlin/graalvm-native.gradle.kts
@@ -48,6 +48,7 @@ graalvmNative {
                 "--initialize-at-build-time=org.slf4j.helpers.NOPLoggerFactory",
                 "--initialize-at-build-time=org.slf4j.helpers.SubstituteLoggerFactory",
                 "--initialize-at-build-time=org.slf4j.helpers.SubstituteServiceProvider",
+                "--initialize-at-build-time=org.slf4j.simple.OutputChoice",
                 "--initialize-at-build-time=org.slf4j.simple.SimpleLogger",
                 "--initialize-at-build-time=org.slf4j.simple.SimpleLoggerConfiguration",
 
