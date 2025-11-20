12a2825b9046e40824f83d7570f42def786c0f4f
Gilles Gosuin <gilles.gosuin@gmail.com>
2025-11-20 06:24:15 +0100

    refactor: remove code duplication

--- app/src/main/kotlin/interfaces/ktor/routes/LegacyRoutes.kt
+++ app/src/main/kotlin/interfaces/ktor/routes/LegacyRoutes.kt
@@ -7,16 +7,17 @@ import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
+private const val NEW_ANIMAL_ROUTE_PATTERN = "/animals/%s"
 private const val PATH_VARIABLE = "path"
 
 fun Application.legacyRoutes() {
     val properties: TemplateProperties by dependencies
 
     val movedResources = mapOf(
-        "/animals/females/{${PATH_VARIABLE}}" to "/animals/%s",
-        "/animals/for-sale/{${PATH_VARIABLE}}" to "/animals/%s",
-        "/animals/geldings/{${PATH_VARIABLE}}" to "/animals/%s",
-        "/animals/sold/{${PATH_VARIABLE}}" to "/animals/%s",
+        "/animals/females/{${PATH_VARIABLE}}" to NEW_ANIMAL_ROUTE_PATTERN,
+        "/animals/for-sale/{${PATH_VARIABLE}}" to NEW_ANIMAL_ROUTE_PATTERN,
+        "/animals/geldings/{${PATH_VARIABLE}}" to NEW_ANIMAL_ROUTE_PATTERN,
+        "/animals/sold/{${PATH_VARIABLE}}" to NEW_ANIMAL_ROUTE_PATTERN,
         "/factsheets/{${PATH_VARIABLE}}" to "${properties.baseAssetUrl}/factsheets/%s",
         "/images/animals/{${PATH_VARIABLE}}" to "${properties.baseAssetUrl}/images/animals/%s",
         "/images/gallery/{${PATH_VARIABLE}}" to "${properties.baseAssetUrl}/images/gallery/%s",
