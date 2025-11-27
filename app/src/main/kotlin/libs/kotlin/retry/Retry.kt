fea4f7f2b6150caf8c909ad543fb66b60cfd738e
Gilles Gosuin <gilles.gosuin@gmail.com>
2025-11-27 07:24:12 +0100

    refactor: remove logging (making it good-looking would be too expensive)

--- app/src/main/kotlin/libs/kotlin/retry/Retry.kt
+++ app/src/main/kotlin/libs/kotlin/retry/Retry.kt
@@ -1,10 +1,6 @@
 package be.alpago.website.libs.kotlin.retry
 
-import be.alpago.website.libs.kotlin.i18n.ordinal
 import kotlinx.coroutines.delay
-import org.slf4j.LoggerFactory
-
-private val logger = LoggerFactory.getLogger("be.alpago.website.libs.kotlin.retry")
 
 suspend fun <T> retry(
     times: Int = 3,
@@ -23,7 +19,6 @@ suspend fun <T> retry(
                 throw e
             }
 
-            logger.warn("Waiting ${delayMillis}ms before retrying an operation that failed for the ${ordinal(currentAttempt)} time", e)
             delay(delayMillis)
             currentAttempt++
             delayMillis *= exponentialBackoffFactor
