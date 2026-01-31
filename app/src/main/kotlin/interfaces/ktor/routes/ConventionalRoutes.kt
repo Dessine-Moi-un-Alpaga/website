4341968dcaa4bf82081477240f0aca67cc15ba58
Gilles Gosuin <gilles.gosuin@gmail.com>
2026-01-31 08:04:55 +0100

    feat: redirect conventional apple touch icon route to the icons directory

--- app/src/main/kotlin/interfaces/ktor/routes/ConventionalRoutes.kt
+++ app/src/main/kotlin/interfaces/ktor/routes/ConventionalRoutes.kt
@@ -5,13 +5,21 @@ import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
+private const val APPLE_TOUCH_ICON = "apple-touch-icon.png"
 private const val FAVICON = "favicon.ico"
 
 fun Application.conventionalRoutes() {
     routing {
-        get("/$FAVICON") {
+        get("/$APPLE_TOUCH_ICON") {
             call.respondRedirect(
                 url = "/assets/icons/$FAVICON",
+                permanent = true,
+            )
+        }
+
+        get("/$FAVICON") {
+            call.respondRedirect(
+                url = "/assets/icons/$APPLE_TOUCH_ICON",
                 permanent = true,
             )
         }
