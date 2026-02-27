b773630771c49d7b4e9885763338ae7114f6f675
Gilles Gosuin <gilles.gosuin@gmail.com>
2026-02-27 08:02:49 +0100

    ci: use gradle and github actions integration features

--- app/settings.gradle.kts
+++ app/settings.gradle.kts
@@ -1,14 +1,1 @@
-plugins {
-    id("com.gradle.develocity") version("4.3.2")
-}
-
 rootProject.name = "website"
-
-develocity {
-    buildScan {
-        publishing.onlyIf { false }
-
-        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
-        termsOfUseAgree.set("yes")
-    }
-}
