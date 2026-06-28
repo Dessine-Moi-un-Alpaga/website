99c0eefa6c8ea9d78c705f511bbf194ce950c80e
Gilles Gosuin <gilles.gosuin@gmail.com>
2026-06-28 09:21:38 +0200

    refactor: make the retry options configurable for RetryingRepositories

--- app/src/main/kotlin/libs/adapters/persistence/RetryingRepository.kt
+++ app/src/main/kotlin/libs/adapters/persistence/RetryingRepository.kt
@@ -2,6 +2,7 @@ package be.alpago.website.libs.adapters.persistence
 
 import be.alpago.website.libs.domain.AggregateRoot
 import be.alpago.website.libs.domain.ports.persistence.Repository
+import be.alpago.website.libs.kotlin.retry.RetryOptions
 import be.alpago.website.libs.kotlin.retry.retry
 
 /**
@@ -9,17 +10,20 @@ import be.alpago.website.libs.kotlin.retry.retry
  *
  * @see retry
  */
-class RetryingRepository<T : AggregateRoot>(private val delegate: Repository<T>): Repository<T> {
+class RetryingRepository<T : AggregateRoot>(
+    private val delegate: Repository<T>,
+    private val options: RetryOptions = RetryOptions(),
+): Repository<T> {
 
-    override suspend fun delete(id: String) = retry { delegate.delete(id) }
+    override suspend fun delete(id: String) = retry(options) { delegate.delete(id) }
 
-    override suspend fun deleteAll() = retry { delegate.deleteAll() }
+    override suspend fun deleteAll() = retry(options) { delegate.deleteAll() }
 
-    override suspend fun findAll() = retry { delegate.findAll() }
+    override suspend fun findAll() = retry(options) { delegate.findAll() }
 
-    override suspend fun findBy(field: String, value: String) = retry { delegate.findBy(field, value) }
+    override suspend fun findBy(field: String, value: String) = retry(options) { delegate.findBy(field, value) }
 
-    override suspend fun get(id: String) = retry { delegate.get(id) }
+    override suspend fun get(id: String) = retry(options) { delegate.get(id) }
 
-    override suspend fun save(aggregateRoot: T) = retry { delegate.save(aggregateRoot) }
+    override suspend fun save(aggregateRoot: T) = retry(options) { delegate.save(aggregateRoot) }
 }
