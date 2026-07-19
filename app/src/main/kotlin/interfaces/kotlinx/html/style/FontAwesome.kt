10bda0d183fe3f4ea4c5580e7b68762d3c94cb62
Gilles Gosuin <gilles.gosuin@gmail.com>
2026-07-19 08:54:21 +0200

    refactor: remove unused constant declaration

--- app/src/main/kotlin/interfaces/kotlinx/html/style/FontAwesome.kt
+++ app/src/main/kotlin/interfaces/kotlinx/html/style/FontAwesome.kt
@@ -4,7 +4,6 @@ object FontAwesome {
 
     const val brands = "brands"
     const val envelope = "fa-envelope"
-    const val facebook = "fa-facebook"
     const val home = "fa-home"
     const val phone = "fa-phone"
     const val social = "fa-comment"
