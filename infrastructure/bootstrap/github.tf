d59965e47aa255e98b627ce6698ed43a0e352685
Gilles Gosuin <gilles.gosuin@gmail.com>
2026-08-04 17:32:14 +0200

    refactor: remove legacy credentials from the bootstrap stack

--- infrastructure/bootstrap/github.tf
+++ infrastructure/bootstrap/github.tf
@@ -21,7 +21,6 @@ resource "random_bytes" "gradle_configuration_cache_encryption_key" {
 locals {
   secrets = {
     API_KEY_HASH          = bcrypt_hash.api_key_hash.id
-    CREDENTIALS           = local.credentials
     GRADLE_ENCRYPTION_KEY = random_bytes.gradle_configuration_cache_encryption_key.base64
     SMTP_SERVER_PASSWORD  = var.smtp_server_password
     SONARCLOUD_TOKEN      = var.sonarcloud_token
