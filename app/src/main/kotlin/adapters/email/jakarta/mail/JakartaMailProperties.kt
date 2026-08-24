db0348055f5972b7725e878962764ee63f033050
Gilles Gosuin <gilles.gosuin@gmail.com>
2026-08-24 20:49:05 +0200

    doc: document the mail properties

--- app/src/main/kotlin/adapters/email/jakarta/mail/JakartaMailProperties.kt
+++ app/src/main/kotlin/adapters/email/jakarta/mail/JakartaMailProperties.kt
@@ -1,9 +1,28 @@
 package com.dessinemoiunalpaga.website.adapters.email.jakarta.mail
 
 data class JakartaMailProperties(
+    /**
+     * The sender email address used by the SMTP server.
+     */
     val address: String,
+
+    /**
+     * The hostname of the SMTP server.
+     */
     val smtpServerAddress: String,
+
+    /**
+     * The password to authenticate with the SMTP server.
+     */
     val smtpServerPassword: String,
+
+    /**
+     * The port the SMTP server uses to listen for connections.
+     */
     val smtpServerPort : Int,
+
+    /**
+     * The username to authenticate with the SMTP server.
+     */
     val smtpServerUsername: String,
 )
