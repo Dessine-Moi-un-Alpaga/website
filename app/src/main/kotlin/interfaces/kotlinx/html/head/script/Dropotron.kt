5a1a6c73e7158f55ac58b03ed8f16d4256945b02
Gilles Gosuin <gilles.gosuin@gmail.com>
2026-09-12 14:15:04 +0200

    refactor: factorize the "no-referrer" referrer policy configuration for script tags

--- app/src/main/kotlin/interfaces/kotlinx/html/head/script/Dropotron.kt
+++ app/src/main/kotlin/interfaces/kotlinx/html/head/script/Dropotron.kt
@@ -4,9 +4,9 @@ import kotlinx.html.*
 
 fun HEAD.dropotron() {
     script {
-        attributes["referrerpolicy"] = "no-referrer"
         crossorigin = ScriptCrossorigin.anonymous
         integrity = "sha512-ugEhUBPC3XfTEBbRia5d9er1tFe5N4yzwQr3xrNSTfmT09xe0fwYxgfDSLwUKCnFoFtLd5rJBZP5tdfcUzLNvw=="
+        noReferrer()
         src = "https://cdnjs.cloudflare.com/ajax/libs/jquery.dropotron/1.4.3/jquery.dropotron.min.js"
     }
 }
