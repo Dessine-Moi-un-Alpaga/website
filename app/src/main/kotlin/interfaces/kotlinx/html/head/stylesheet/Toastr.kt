2813cd787d2fd00a87beae1aac70dbe899c47e81
Gilles Gosuin <gilles.gosuin@gmail.com>
2026-09-12 14:24:18 +0200

    refactor: factorize the referrer configuration for link tags

--- app/src/main/kotlin/interfaces/kotlinx/html/head/stylesheet/Toastr.kt
+++ app/src/main/kotlin/interfaces/kotlinx/html/head/stylesheet/Toastr.kt
@@ -4,8 +4,7 @@ import kotlinx.html.*
 
 fun HEAD.toastrStylesheet() {
     link {
-        attributes["crossorigin"] = "anonymous"
-        attributes["referrerpolicy"] = "no-referrer"
+        externalStylesheetAttributes()
         integrity = "sha512-6S2HWzVFxruDlZxI3sXOZZ4/eJ8AcxkQH1+JjSe/ONCEqR9L4Ysq5JdT5ipqtzU7WHalNwzwBv+iE51gNHJNqQ=="
         rel = LinkRel.stylesheet
         href = "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css"
